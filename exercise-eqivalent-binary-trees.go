b3537146781773d6ae0cd9b60254a57e65a4dfc0
YamagishiRyu <bubusu.ryu@gmail.com>
2018-08-14 22:24:03 +0900

    Add: equivalent binary tree

---
+++ exercise-eqivalent-binary-trees.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
+
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := 0; i < 10; i++ {
+		x, y := <-ch1, <-ch2
+		if x != y {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+}
